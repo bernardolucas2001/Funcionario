a3e0bfaeab9842ce51fc7d09f8a6689c88a827fc
Bernardo Lucas de Sousa <bernardosaopaulino@gmail.com>
2022-12-27 21:49:15 -0300

    exercicio de vetores

---
+++ Salario/Funcionario.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Salario
+{
+     class Funcionario
+    { 
+        public string Nome { get; set; }
+        public double Salario { get; private set; }
+
+        public Funcionario(string nome, double salario )
+        {
+            Nome = nome;
+            Salario = salario;
+        }
+        public override string ToString()
+        {
+            return Nome + " , Salario: " + Salario;
+        }
+    }
+}
---
+++ Salario/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Globalization;
+
+namespace Salario
+{
+    class Tabela
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Quantos funcionarios vão ser cadastrado? ");
+            int n = int.Parse(Console.ReadLine());
+            Funcionario[] vect = new Funcionario[50];
+
+            for (int i = 1; i <= n; i++) {
+                Console.WriteLine();
+                Console.Write("Nome: ");
+                string nome = Console.ReadLine();
+                Console.Write("Salario: ");
+                double salario = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                Console.Write("Numero do funcionario: ");
+                int func = int.Parse(Console.ReadLine());
+                vect[func] = new Funcionario(nome, salario);
+
+            }
+            Console.WriteLine();
+            Console.WriteLine("Funcionarios cadastrados:");
+            for (int i = 0; i < 50; i++)
+            {
+                if (vect[i] != null)
+                {
+                    Console.WriteLine(i + ": " + vect[i]);
+
+
+
+
+                }
+
+            }
+
+        }
+    }
+}
